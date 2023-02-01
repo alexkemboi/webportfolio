743bc283fb284f7f85215839a7e9205e470e010f
alexkemboi4127 <alexkemboi97@yahoo.com>
2023-02-01 21:35:33 +0300

    fixed the navbar and project protfolio

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
+const header = document.querySelector(".navbar");
 
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >= 100) {
+    header.classList.add("navbarDark");
+  } else {
+    header.classList.remove("navbarDark");
+  }
+};
+
+function getUserRepos() {
+  const repos = fetch(`https://api.github.com/users/alexkemboi/repos`);
+  console.log(repos);
+  return repos;
 }
-// async function getUserRepos() {
-//     const repos = await fetch(`https://api.github.com/users/alexkemboi/repos`);
-//     return repos;
-//  }
- 
-//  getUserRepos()
-//        .then(repos => {
-//             console.log(repos);
-//   }); 
-  var request = new XMLHttpRequest();
-        request.open('GET','https://api.github.com/users/alexkemboi/repos' , 
-        true)
-        request.onload = function() {
-            var data = JSON.parse(this.response);
-            console.log(data);
-            var statusHTML = '';
-            $.each(data, function(i, status){
-                if(status.description){
-                    statusHTML += 
-                    '<div style="padding:5px;justify-content:center;align-items:center;">\
-                        <h4 class="text-center">'+ status.name.charAt(0).toUpperCase() + status.name.slice(1)+'</h4>\
-                        <p>'+ status.description +'</p>\
-                        <button class="btn btn-dark"><a class="fa fa-github" href="'+status.html_url+'">Open Repo</a></button>\
-                        <p>Language:'+ status.language+'</p>\
-                    </div>';                    
-                }               
-            });
-            $('.repositories').html(statusHTML);
-        }
-        
-        request.send();
 
+getUserRepos();
 
-        //skills js
-        $(".bar").each(function(){
-            $(this).find(".bar-inner").animate({
-              width: $(this).attr("data-width")
-            },2000)
-          });
+var request = new XMLHttpRequest();
+request.open("GET", "https://api.github.com/users/alexkemboi/repos", true);
+request.onload = function () {
+  var data = JSON.parse(this.response);
+  console.log(data);
+  var statusHTML = "";
+  $.each(data, function (i, status) {
+    if (status.description) {
+      statusHTML += `<div class="col-12 col-md-3">
+    <div class="card m-1">
+        <img src="/images/portfolio2.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h3 class="card-title text-sm">${
+              status.name.charAt(0).toUpperCase() + status.name.slice(1)
+            }</h3>
+            <p class="card-text text-sm">Description:${status.description}</p>
+            <button class="btn btn-dark text-white" type="button"><a href="${
+              status.html_url
+            }" class="">Open repo</a><i></i> </button>
+        </div>
+    </div>        
+</div>`;
+    }
+  });
+  $(".repositories").html(statusHTML);
+};
+
+request.send();
+
+//skills js
+$(".bar").each(function () {
+  $(this)
+    .find(".bar-inner")
+    .animate(
+      {
+        width: $(this).attr("data-width"),
+      },
+      2000
+    );
+});
